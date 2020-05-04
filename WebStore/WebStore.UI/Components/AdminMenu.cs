3b78cda001fc62b06fa3762c5938cdca46a93561
Yavnikov Roman <romazes20@gmail.com>
2020-05-04 08:21:27 +0300

    8. Enabled Security - Part 2: User Administration
    8.4. Enabled User Management

---
+++ WebStore/WebStore.UI/Components/AdminMenu.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebStore.UI.Components
+{
+    public class AdminMenu : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var menuItems = new List<AdminMenuItem> {
+                new AdminMenuItem()
+                {
+                    DisplayValue = "User management",
+                    ActionValue = "UserManagement"
+
+                }};
+
+            return View(menuItems);
+        }
+    }
+
+    public class AdminMenuItem
+    {
+        public string DisplayValue { get; set; }
+        public string ActionValue { get; set; }
+    }
+}
